b67c47d82503b155a7fe345534ea6f950fe9cf34
[h61781jp] <[jack.pay@student.manchester.ac.uk]>
2021-10-15 12:01:10 +0100

    Created a single simple button that does nothing

--- game_V1.py
+++ game_V1.py
@@ -2,9 +2,15 @@ import tkinter
 from tkinter import * 
 # Will import everything from the tkinter library
 
-window = Tk() #Creates a window
+windowMenu = Tk() #Creates a window
+windowMenu.geometry("500x500")#Sets the size of the window
+windowMenu.title("Menu") #Sets the title of the window currently
 
-window.mainloop() #Will put the window into a listening mode so that it waits for an event to happen. Without it will instantly close, this ensures the program remains open
+#Buttons
+btnLoadGame = Button(windowMenu,text = "Load Game")#Creates a basic button displaying text
+btnLoadGame.place(relx = 0.5, rely = 0.2, anchor = CENTER)#Will place the button relative to 0.1 of the width of the screen, 0,5 of the height of the screen and in the center
+
+windowMenu.mainloop() #Will put the window into a listening mode so that it waits for an event to happen. Without it will instantly close, this ensures the program remains open
 
 
 
