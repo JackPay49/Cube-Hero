6c2cda8db937a8bf7610305828aa28345dc8e051
[h61781jp] <[jack.pay@student.manchester.ac.uk]>
2021-10-15 11:02:24 +0100

    Made a blank window

---
+++ game_V1.py
@@ -0,0 +1,10 @@
+import tkinter
+from tkinter import * 
+# Will import everything from the tkinter library
+
+window = Tk() #Creates a window
+
+window.mainloop() #Will put the window into a listening mode so that it waits for an event to happen. Without it will instantly close, this ensures the program remains open
+
+
+
