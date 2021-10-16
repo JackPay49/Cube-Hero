333783a77d96e09d17935a38f8fad0e327b32810
[h61781jp] <[jack.pay@student.manchester.ac.uk]>
2021-10-16 14:51:35 +0100

    Created a very basic player class with the ability to load in data from a text file. Added the scoreboard text file to load in actual player records

--- game_V1.py
+++ game_V1.py
@@ -7,7 +7,23 @@ import tkinter.font as font #This will import the font changing part of tkinter 
 #Classes
 # class snake:
 
-# class player:
+class player:
+	name = ""#All variables or attributes of players
+	level = 0
+	score = 0
+	# playerSnake = snake()
+
+	def LoadPlayer(self,nValue):
+		name = nValue
+		file = open("gameFiles/" + name + ".txt","rt")#This will open a file for reading in the text format based on the input name of the user
+		
+		file.readline()#Used to skip the name which is written into the file for readability
+		level = int(file.readline())#Gets their level and score & converts them into integers
+		score = int(file.readline())
+		file.close()#Must close the file afterwards!
+
+	# def SavePlayer():
+		#Code to write a text file for the player
 
 #Setting up Windows
 def SetUpMenu(windowMenu):
@@ -42,6 +58,7 @@ def SetUpMenu(windowMenu):
 	lbTitle.place(relx = 0.5, rely = 0.1, anchor = CENTER)
 	lbTitle['font'] = fontTitle
 
+#All scoreboard stuff
 def SetUpScoreboard(windowScoreboard):
 	windowScoreboard.geometry("1000x700")
 	windowScoreboard.title("Scoreboard")
@@ -52,40 +69,51 @@ def SetUpScoreboard(windowScoreboard):
 	lbTitle.place(relx = 0.5, rely = 0.1, anchor = CENTER)
 	lbTitle['font'] = fontTitle
 
+
 	btnClose = Button(windowScoreboard,text = "Back",command = windowScoreboard.destroy)
 	btnClose.place(relx = 0.5, rely = 0.9, anchor = CENTER)
 
 	scoreBox = Text(windowScoreboard,height = 30, width = 80)#This makes a simple textbox for the scoreboard with the attributes mentioned
 	scoreBox.place(relx = 0.5,rely = 0.5,anchor = CENTER)
-	# scoreBox.config(state = "disabled")#Will disable the text box so that the user cannot change what's in it
 
-	player1 = {"name":"Jack","score":1000}#Test data below
-	player2 = {"name":"Mark","score":100}
-	player3 = {"name":"Phil","score":10}
-	scores = [player1,player3,player2]
+	scores = []#Creates a blank list to put players into
+	LoadInScoreboard(scores)#Will fill in the scores list, putting in the players on the scoreboard
 	SortScores(scores)#Ensures to order the data incase it isn't currently in order
 
-	numberOfScores = 3
 	scoreText = ""
-	for i in range(0 ,numberOfScores):
+	for i in range(0 ,len(scores)):#Loops through every single score in the scorebaord and displays them. This way incase the number of scores on the board changes
 		scoreBox.insert(INSERT,str(i + 1) + ".")#Will add the scoreboard text to the text box. The INSERT is the location of where to insert the text. This means to just insert it to the end
 		#The part above will add the 1. or 2. or n. to each score to signify where they are in the rank
-		scoreBox.insert(INSERT,scores[i].get("name") + " : " + str(scores[i].get("score")))#Will print their name and then score
+		scoreBox.insert(INSERT,scores[i].name + " : " + str(scores[i].score))#Will print their name and then score
 		for j in range(0,3):
 			scoreBox.insert(INSERT, "\n")#Will insert 3 lines between text to ensure each player entry is really spaced out
 
 
+
 def SortScores(scores):
 	for i in range(0,len(scores)):
-		print("Sort Scores!") #Placeholder for now
+		continue
 
-
+def LoadInScoreboard(scores):
+	file = open("gameFiles/scoreboard.txt","rt")
+	numberOfScores = int(file.readline())
+	for i in range(0,numberOfScores):
+		tempPlayer = player()#Will create a dumby player to store within the scores list
+		tempPlayer.name = file.readline().strip()#Loads in their name and score, on two separate lines. Strip is used to get rid of any leading or trailing white space.
+		tempPlayer.score = int(file.readline())
+		file.readline()#Will then skip one line, each player record has a space in between for readability
+		scores.append(tempPlayer)#Will add the player to the scoreboard
+	file.close()#Must close file!
 
 def OpenScoreboard():
 	windowScoreboard = Tk()#Creates the scoreboard window
 	SetUpScoreboard(windowScoreboard)#Will run and set up all textboxes for the scoreboard, Will also load in all of the players on the scoreboard
 	windowScoreboard.mainloop()
 
+
+
+
+#Main sub that runs the program
 def BeginGame():
 	windowMenu = Tk() #Creates a window
 	SetUpMenu(windowMenu) #Will run the procedure and set up the menu correctly
