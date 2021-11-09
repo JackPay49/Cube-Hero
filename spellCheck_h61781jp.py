0e52314ac3e9a6fb6782b824866c879acc2c45e4
[h61781jp] <[jack.pay@student.manchester.ac.uk]>
2021-11-09 17:44:47 +0000

    Made changes based on FAQs

--- spellCheck_h61781jp.py
+++ spellCheck_h61781jp.py
@@ -1,4 +1,4 @@
-import sys
+import sys,os
 
 dictionary = []
 file = open(sys.argv[1],"r")
@@ -7,59 +7,63 @@ for line in file:
 	dictionary.append(line)
 file.close()
 
-file = open(sys.argv[2],"r")
-fullString = file.read()
-file.close()
 
-nPunctuation = 0
-nUpperCase = 0
-nNumbers = 0
-nWords = 0
-nCorrectWords = 0
-allwords = []
+for filename in os.listdir(sys.argv[2]):
+	file = open((sys.argv[2] + filename),"r")
+	fullString = file.read()
+	file.close()
 
-uppcaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+	nPunctuation = 0
+	nUpperCase = 0
+	nNumbers = 0
+	nWords = 0
+	nCorrectWords = 0
+	allwords = []
 
-currentWord = ""
-for i in range(len(fullString)):
-	addWord = False
-	if (fullString[i] in ("!","?",",",".","(",")",":",";","[","]",'"',"'","-","/","@","{","}","*","|","£","$","%","^","=","+","_","~","#","<",">",)):
-		nPunctuation +=1
-		if (fullString[i] not in ("'","-")):
-			addWord = True
-	elif (fullString[i] in ("1","2","3","4","5","6","7","8","9","0")):
-		nNumbers +=1
-		addWord = True
-	elif (fullString[i] in uppcaseLetters):
-		nUpperCase += 1
-		currentWord += fullString[i].lower()
-	elif (fullString[i] == " "):
-		addWord = True
-	else:
-		currentWord += fullString[i]
+	uppcaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-	if (currentWord == ""):
+	currentWord = ""
+	for i in range(len(fullString)):
 		addWord = False
-	elif(i == len(fullString) - 1):
-		addWord = True
+		if (fullString[i] in ("!","?",",",".","(",")",":",";","[","]",'"',"'","-","/","@","{","}","*","|","£","$","%","^","=","+","_","~","#","<",">",)):
+			nPunctuation +=1
+			if (fullString[i] not in ("'","-")):
+				addWord = True
+		elif (fullString[i] in ("1","2","3","4","5","6","7","8","9","0")):
+			nNumbers +=1
+			addWord = True
+		elif (fullString[i] in uppcaseLetters):
+			nUpperCase += 1
+			currentWord += fullString[i].lower()
+		elif (fullString[i] == " "):
+			addWord = True
+		else:
+			currentWord += fullString[i]
 
-	if (addWord):
-		nWords +=1
-		allwords.append(currentWord.strip())
-		currentWord = ""
+		if (currentWord == ""):
+			addWord = False
+		elif(i == len(fullString) - 1):
+			addWord = True
 
-for i in range(nWords):
-	if (allwords[i] in dictionary):
-		nCorrectWords +=1
+		if (addWord):
+			nWords +=1
+			allwords.append(currentWord.strip())
+			currentWord = ""
 
-file = open(sys.argv[3],"w")
-file.write("h61781jp\n")
-file.write("Formatting ###################\n")
-file.write("Number of upper case words changed: " + str(nUpperCase) + "\n")
-file.write("Number of punctuations removed: " + str(nPunctuation)+ "\n")
-file.write("Number of numbers removed: " + str(nNumbers)+ "\n")
-file.write("Spellchecking ###################\n")
-file.write("Number of words: " + str(nWords) + "\n")
-file.write("Number of correct words: " + str(nCorrectWords) + "\n")
-file.write("Number of incorrect words: " + str(nWords - nCorrectWords) + "\n")
-file.close()
+	for i in range(nWords):
+		if (allwords[i] in dictionary):
+			nCorrectWords +=1
+
+	newFileName = filename[0:(len(filename) - 4):1]
+	file = open((sys.argv[3] + newFileName + "_h61781jp.txt"),"w")
+
+	file.write("h61781jp\n")
+	file.write("Formatting ###################\n")
+	file.write("Number of upper case words changed: " + str(nUpperCase) + "\n")
+	file.write("Number of punctuations removed: " + str(nPunctuation)+ "\n")
+	file.write("Number of numbers removed: " + str(nNumbers)+ "\n")
+	file.write("Spellchecking ###################\n")
+	file.write("Number of words: " + str(nWords) + "\n")
+	file.write("Number of correct words: " + str(nCorrectWords) + "\n")
+	file.write("Number of incorrect words: " + str(nWords - nCorrectWords) + "\n")
+	file.close()
